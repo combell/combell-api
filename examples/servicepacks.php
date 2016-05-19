f77dd8d0e9e4114d211bf87f8d186b15e0046e78
Bram Dejonckheere <bram@combellgroup.com>
2016-05-19 16:40:35 +0200

    List service packs

---
+++ examples/servicepacks.php
@@ -0,0 +1,20 @@
+<?php
+
+require dirname(__DIR__) . '/vendor/autoload.php';
+
+$client = new \Combell\Client(
+    [
+        'debug' => true,
+        'base_uri' => 'https://api.combell.com',
+        'combell_api_key' => 'XXXX',
+        'combell_api_secret' => 'YYYY'
+    ]
+);
+
+// Get all servicepacks
+$response = $client->get('/v1/servicepacks');
+
+// Dump response
+var_dump(
+    json_decode($response->getBody()->getContents())
+);
